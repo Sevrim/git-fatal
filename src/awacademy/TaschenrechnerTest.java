68091309aac5d68403222d05ef150d0c24ff8b06
Sevrim <68111172+Sevrim@users.noreply.github.com>
2020-08-11 14:33:35 +0200

    Korrektur vom Code

--- src/awacademy/TaschenrechnerTest.java
+++ src/awacademy/TaschenrechnerTest.java
@@ -55,8 +55,29 @@ public class TaschenrechnerTest {
 			int maxIntegerPlusEins 	= Integer.MAX_VALUE + 1;
 		
 			System.out.println(maxIntegerPlusEins);	
+			
 				
 		}
-	}
 
+		@Test
+		public void multiplikation () {
+
+			TaschenrechnerOlli meinTaschenrechner = new TaschenrechnerOlli ();
+
+			double aArray []= {-2, 3, -5, 6, -1, -2, 1};
+			
+			double bArray [] = {-8, 7, 4, -6, 3, 3, -2};
+			
+
+			
+			for (double aWert: aArray) {
+				System.out.println("==> a:" + aWert);
+				
+			for (double bWert: bArray) {
+				System.out.println("\n" + "==> " + aWert + "*" + bWert );				
+				System.out.println("=" + aWert * bWert);
+			}	
+		}
+	}
+}
 
